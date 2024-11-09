0b7c261448cb5b73a17110e9e9f5d98994504173
OrrMak <makonnenrocks@gmail.com>
2024-11-09 22:06:44 +0200

    TheDice

---
+++ src/components/TheDice.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function TheDice() {
+  return (
+    <div>
+        <img src={`../assets/images/dice-${diceRes}.png`} alt={`dice-${diceRes}`}/>
+        <img src={`../assets/images/dice-${diceRes}.png`} alt={`dice-${diceRes}`}/>
+    </div>
+  )
+}
+
+export default TheDice
+
+
