8417c134e35eea16e16094c6283dafc8fc6a5544
OrrMak <makonnenrocks@gmail.com>
2024-11-09 22:41:35 +0200

    RollDiceButton

---
+++ src/components/RollDiceButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function RollDiceButton() {
+  return (
+    <div>
+        <button id='RollDiceButton'>ROLL DICE</button>
+    </div>
+  )
+}
+
+export default RollDiceButton;
